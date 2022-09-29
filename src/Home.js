99010bea903eb4e5bbca4989e246c35c784d9158
ChamodiDH <chamodi3797@gmail.com>
2022-09-29 20:35:14 +0530

    Home page UI

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Home(){
+
+    return(
+        <div className='Home'>
+
+            <h1 className='home-header'>Quizzical</h1>
+            <p className='home-desc'>Take the quiz and test your knowledge!</p>
+            <button className='home-btn-start'>Start Quiz</button>
+
+
+        </div>
+    )
+
+}
